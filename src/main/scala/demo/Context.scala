e86c70276a8432db37957efd91ffb7ddc87d5e34
Alexey Shcherbakov <a.m.shcherbakov@tinkoff.ru>
2018-01-20 19:24:56 +0300

    initial

--- src/main/scala/demo/Context.scala
+++ src/main/scala/demo/Context.scala
@@ -7,7 +7,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 class Context(appName: String) extends LazyLogging {
   val sparkConfig: SparkConf = {
     val conf = new SparkConf().setAppName(appName)
-    Option(System.getenv("SPARK_MASTER_URL")) match {
+    Option(System.getenv("SPARK_MASTER_URL")) match { // https://github.com/big-data-europe/docker-spark
       case Some(_) => conf
       case None => conf.setMaster("local[*]")
     }
