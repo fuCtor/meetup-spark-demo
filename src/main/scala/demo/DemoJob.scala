50bfe2fe24a4c4ebc43fa35fe8c16b2c9dfffb68
Alexey Shcherbakov <a.m.shcherbakov@tinkoff.ru>
2018-01-20 19:45:04 +0300

    Read data

--- src/main/scala/demo/DemoJob.scala
+++ src/main/scala/demo/DemoJob.scala
@@ -1,7 +1,20 @@
 package demo
 
+import org.apache.spark.sql.DataFrame
+
 object DemoJob extends App {
-  val appContext = Context("demo_app")
+  implicit val appContext = Context("demo_app")
+
+  val rating = readCSV("data/ratings_small.csv")
+  val moviesMetadata = readCSV("data/movies_metadata.csv")
+
+  rating.show(10)
+  moviesMetadata.show(10)
+
+  moviesMetadata.printSchema()
 
   appContext.stop()
+
+  def readCSV(path: String)(implicit context: Context): DataFrame =
+    context.spark.read.option("header", "true").csv(path)
 }
